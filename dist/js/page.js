55327979475b6f959df4d50a08eeb956038caf6f
SergheiNiculaev <spam312sn@gmail.com>
2018-05-17 03:11:53 +0300

    Personal web-page redesign

---
+++ dist/js/page.js
@@ -0,0 +1,4 @@
+document.addEventListener('DOMContentLoaded', function () {
+    M.FloatingActionButton.init(document.querySelectorAll('.fixed-action-btn'))
+    M.TapTarget.init(document.querySelector('.tap-target')).open()
+})
